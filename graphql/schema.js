edccda6976df5ebb06ebe5216025ea1ff35c5107
Ivan Gil <igil.dev@gmail.com>
2019-02-03 13:28:19 -0500

    create user mutation defined

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,16 +1,34 @@
-const {buildSchema}  = require('graphql');
+const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-type TestData {
-    text: String!
-    views: Int!
+type Post {
+    _id: ID!,
+    content: String!,
+    creator: User!
+    createdAt: String!
+    updatedAt: String!
 }
 
-type RootQuery {
-    hello: TestData
+type User {
+    _id: ID!
+    name: String!
+    email: String!
+    password: String
+    status: String!
+    posts: [Post!]!
+}
+
+input userDataInput {
+    email: string!
+    name: string!
+    password: string!
+}
+
+type RootMutation {
+    createUser(userInput: userDataInput): User!
 }
 
 schema {
-    query: RootQuery
+    mutation: RootMutation
 }
-`)
+`);
