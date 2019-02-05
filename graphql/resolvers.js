291e048bd968487898b1d1d2ed4b853ec3f9bc09
Ivan Gil <igil.dev@gmail.com>
2019-02-04 22:09:12 -0500

    validation added to create user

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,33 @@
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const User = require('../models/user');
 
 module.exports = {
   createUser: async function({ userInput }, req) {
-  
+    const errors = [];
+
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: 'Email not valid!' });
+    }
+
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push('Passord empty or not valid');
+    }
+    if (
+      validator.isEmpty(userInput.name) ||
+      !validator.isLength(userInput.name, { min: 3 })
+    ) {
+      errors.push('Name empty or not valid');
+    }
+
+    if (errors.length > 0) {
+      throw new Error('Input not Valid!');
+    }
+
     const userExists = await User.findOne({ email: userInput.email });
 
     if (userExists) {
@@ -14,7 +37,7 @@ module.exports = {
     const hashPw = await bcrypt.hash(userInput.password, 12);
 
     const user = new User({
-      email: userInput.email,
+      email: validator.normalizeEmail(userInput.email),
       name: userInput.name,
       password: hashPw
     });
