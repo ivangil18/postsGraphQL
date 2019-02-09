fd894924f019bbe579d9dfd41af21e6d29d03880
Ivan Gil <igil.dev@gmail.com>
2019-02-09 08:56:20 -0500

    createPost mutation  implemented

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -54,7 +54,6 @@ module.exports = {
 
   login: async function({ email, password }) {
     const user = await User.findOne({ email: email });
-     
 
     if (!user) {
       const error = new Error('User not found!');
@@ -79,5 +78,63 @@ module.exports = {
     console.log({ token: token, userId: user._id.toString() });
 
     return { token: token, userId: user._id.toString() };
+  },
+
+  createPost: async function({ postInput }, req) {
+    if (!req.isAuth) {
+      const error = new Error('User not authenticated!');
+      error.code = 401;
+      throw error;
+    }
+
+    const errors = [];
+
+    if (
+      validator.isEmpty(postInput.title) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: 'Title not valid!' });
+    }
+
+    if (
+      validator.isEmpty(postInput.content) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: 'Content not valid!' });
+    }
+
+    if (errors.length > 0) {
+      const error = new Error('Invalid Inputs');
+      error.code = 422;
+      error.data = errors;
+      throw error;
+    }
+
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      const error = new Error('User not found');
+      error.code = 401;
+      error.data = errors;
+      throw error;
+    }
+
+    const post = new Post({
+      title: postInput.title,
+      content: postInput.content,
+      imageUrl: postInput.imageUrl,
+      creator: user
+    });
+
+    user.posts.push(post);
+    const result = user.save();
+    const postCreated = await post.save();
+
+    return {
+      ...postCreated._doc,
+      _id: postCreated._id.toString(),
+      createdAt: postCreated.createdAt.toISOString(),
+      updatedAt: postCreated.updatedAt.toISOString()
+    };
   }
 };
